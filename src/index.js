5b34e7973642589b6c2295e61c5015be7dda2f5a
Vagyasri <vagyasri2011@gmail.com>
2021-11-04 18:07:35 +0100

    Create function for detecting prime numbers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+// Count the number of ways you can reduce a deletable prime to a one digit number
+
+const prime = (n) => {
+  if (n === 2) return true;
+  if (n % 2 === 0 || n <= 1) return false;
+
+  for (let i = 3; i < Math.sqrt(n); i += 2) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+let num = 412567;
+var digits = num.toString().split('');
+var realDigits = digits.map(Number);
+
+console.log(realDigits);
+let count = 0;
+
+// console.log(prime(412567));
